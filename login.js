aca3dfa60e236581844ccbf0cfb0a7a808bce3f4
Leonardo <leo.kawachi@gmail.com>
2024-05-23 16:30:16 -0300

    So falta arrumar a storage agr

--- login.js
+++ login.js
@@ -1,24 +1,24 @@
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const username = form.username.value
-    const password = form.password.value
+  const username = form.username.value;
+  const password = form.password.value;
 
-    const authenticated = authentication(username,password)
+  const authenticated = authentication(username, password);
 
-    if(authenticated){
-        window.location.href = "logout.html"
-    }else{
-        alert("Digite os campos corretamente!")
-    }
-})
+  if (authenticated) {
+    window.location.href = "logout.html";
+  } else {
+    alert("Digite os campos corretamente!");
+  }
+});
 
-function authentication(username,password){
-    if(username === "admin@email.com" && password === "admin"){
-        return true
-    }else{
-        return false
-    }
+function authentication(username, password) {
+  if (username === "admin@email.com" && password === "admin") {
+    return true;
+  } else {
+    return false;
+  }
 }
---
+++ storage.js
@@ -0,0 +1,5 @@
+const usuario = JSON.parse(localStorage.getItem("usuario-validado"));
+console.log(usuario.emailUsuario);
+
+const chaveString = localStorage.getItem("chave");
+console.log(chaveString);
